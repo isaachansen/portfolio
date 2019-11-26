8cfd043437fb33602a9ceb38a9bfaf30d3b8801d
Isaac Hansen <isaac@Isaacs-MacBook-Pro-2.local>
2019-11-26 15:16:05 -0700

    added basic project layout

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Landing from "./Components/Landing/Landing";
+import Projects from "./Components/Projects/Projects";
 import { Route, Switch } from "react-router-dom";
 
 class App extends React.Component {
@@ -9,8 +10,8 @@ class App extends React.Component {
       <div className="App">
         {/* <Landing /> */}
         <Switch>
-          <Route exact path="/" component={Landing}/>
-          <Route path="/projects" />
+          <Route exact path="/" component={Landing} />
+          <Route path="/projects" component={Projects}/>
         </Switch>
       </div>
     );
--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
-
+import { TiArrowLeftThick } from "react-icons/ti";
+import { NavLink } from "react-router-dom";
+import "./Projects.scss";
 
 export default class Projects extends Component {
-    render() {
-        return(
-            <div></div>
-        )
-    }
+  render() {
+    return (
+      <div className="projects-background">
+          <NavLink to='/'>
+        <TiArrowLeftThick className="arrow" />
+          </NavLink>
+        <div className="project-grid">
+            <div className="project-1">
+                <div className="img-loc"></div>
+                <div className="paragraph"></div>
+                <div className="pro-links"></div>
+            </div>
+            <div className="project-2">
+                <div className="img-loc"></div>
+                <div className="paragraph"></div>
+                <div className="pro-links"></div>
+            </div>
+            <div className="project-3">
+                <div className="img-loc"></div>
+                <div className="paragraph"></div>
+                <div className="pro-links"></div>
+            </div>
+        </div>
+      </div>
+    );
+  }
 }
