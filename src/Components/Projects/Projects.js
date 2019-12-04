f06a54424744672199c3a34bb805f7a1bd51dc92
Isaac Hansen <isaac@Isaacs-MacBook-Pro-2.local>
2019-12-03 21:18:45 -0700

    projects changes

--- src/Components/Projects/Projects.js
+++ src/Components/Projects/Projects.js
@@ -19,14 +19,14 @@ export default class Projects extends Component {
             </div>
             <div className="paragraph">
               <h1 className="proj-title">QNDP</h1>
-              <h3 className="proj-text">A minimalistic static site used by QNDP, a parent company for smaller buisnesses.</h3>
+              <h3 className="proj-text">* NOT RELEASED YET * <br></br>  A minimalistic static site used by QNDP, a parent company for smaller buisnesses.</h3>
               <h3 className="proj-text">Created with React</h3>
             </div>
             <div className="pro-links">
               <a href="https://github.com/isaachansen/QNDP-site">
                 <button className="proj-btn">VIEW CODE</button>
               </a>
-              <button className="proj-btn">LIVE SITE</button>
+              {/* <button className="proj-btn">LIVE SITE</button> */}
             </div>
           </div>
           <div className="project-2">
